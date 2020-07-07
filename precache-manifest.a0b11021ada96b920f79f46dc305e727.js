458f42c400ccee56fb77bd792ff41164de80ae5e
shiwani2626sinha <shiwani2626@gmail.com>
2020-07-07 18:05:18 +0530

    Updates

---
+++ precache-manifest.a0b11021ada96b920f79f46dc305e727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc263abc46506af26de19a5b57ed2c4",
+    "url": "/shiwani2626sinha/React-to-do-list/index.html"
+  },
+  {
+    "revision": "44dd98312e7928c1d0b7",
+    "url": "/shiwani2626sinha/React-to-do-list/static/css/main.bbe653aa.chunk.css"
+  },
+  {
+    "revision": "a3bd34f65802660545a6",
+    "url": "/shiwani2626sinha/React-to-do-list/static/js/2.435f18f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shiwani2626sinha/React-to-do-list/static/js/2.435f18f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44dd98312e7928c1d0b7",
+    "url": "/shiwani2626sinha/React-to-do-list/static/js/main.f7850707.chunk.js"
+  },
+  {
+    "revision": "c4b28f45c5be3b7c5b8e",
+    "url": "/shiwani2626sinha/React-to-do-list/static/js/runtime-main.4c50f09e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shiwani2626sinha/React-to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
